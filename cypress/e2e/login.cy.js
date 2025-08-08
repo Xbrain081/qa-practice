5ad8e6ce7396bf6262018f73fe0e9f52a5a84d5f
Danny_Ibague <xbrain081@gmail.com>
2025-08-08 17:35:31 -0500

    pruebas de comprobaciones realizadas en el login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,11 +3,35 @@ describe('Prueba de Login - The Internet', () => {
       cy.visit('https://www.saucedemo.com/v1/index.html');
     });
   
-    it('Debería iniciar sesión exitosamente con credenciales válidas', () => {
+    it('usuario ingresa con credenciales validas caso positivo', () => {
       cy.get('[data-test="username"]').should('exist').should('be.visible');
       cy.get('[data-test="password"]').should('exist').should('be.visible');
       cy.get('[data-test="username"]').type('standard_user');
       cy.get('[data-test="password"]').type('secret_sauce');
       cy.get('#login-button').should('exist').should('not.be.disabled').click();
     });
+
+    it('usuario ingresa con usuario bloqueado', () => {
+      cy.get('[data-test="username"]').should('exist').should('be.visible');
+      cy.get('[data-test="password"]').should('exist').should('be.visible');
+      cy.get('[data-test="username"]').type('locked_out_user');
+      cy.get('[data-test="password"]').type('secret_sauce');
+      cy.get('#login-button').should('exist').should('not.be.disabled').click();
+      cy.get('[data-test="error"]').should('exist').should('be.visible').and('contain.text', 'Epic sadface: Sorry, this user has been locked out.');
+    });
+
+    it('usuario da click en login sin un usuario valido', () => {
+      cy.get('#login-button').should('exist').should('not.be.disabled').click();
+      cy.get('[data-test="error"]').should('exist').should('be.visible').and('contain.text', 'Epic sadface: Username is required');
+    });
+
+    it('usuario trata de ingresar con una contraseña invalida', () => {
+      cy.get('[data-test="username"]').should('exist').should('be.visible');
+      cy.get('[data-test="password"]').should('exist').should('be.visible');
+      cy.get('[data-test="username"]').type('standard_user');
+      cy.get('[data-test="password"]').type('123');
+      cy.get('#login-button').should('exist').should('not.be.disabled').click();
+      cy.get('[data-test="error"]').should('exist').should('be.visible').and('contain.text', 'Epic sadface: Username and password do not match any user in this service');
+    });
+
   });
